7cd813bb752e1f17f9ee97e3739391d76dbbdb92
dstanchevici <distanchev@gmail.com>
2023-03-26 15:40:46 -0400

    4.17

---
+++ ExpoPoly.java
@@ -0,0 +1,14 @@
+
+public class ExpoPoly {
+
+    public static void main (String[] argv)
+    {
+	for (double n=10; n<=100; n+=10) {
+	    // WRITE YOUR CODE HERE
+	    double poly = Math.pow (n, 4);                 
+            double exp = Math.pow (2.0, n);  
+	    System.out.printf ("poly=%f  exp=%f\n", poly, exp);
+	}
+    }
+
+}
